2d6528beea46c195152b66541a6d8084be971b0e
Almaz Galimov <almazwork@gmail.com>
2024-02-12 18:52:23 +0500

    add services.py

---
+++ services/services.py
@@ -0,0 +1,17 @@
+categories = {'english': 0, 'golang': 0, 'python': 0, 'django': 0, 'swift': 0, 'team leading': 0}
+
+
+def get_categories():
+    return sorted(categories.keys())
+
+
+def add_category(category):
+    categories.setdefault(category, 0)
+
+
+def remove_category(category):
+    categories.pop(category, None)
+
+
+def get_statics():
+    return sorted(categories)
