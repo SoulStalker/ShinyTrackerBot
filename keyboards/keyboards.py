7b1b624893072f1f34886671d8505257cb8668ef
Almaz Galimov <almazwork@gmail.com>
2024-02-16 11:10:02 +0500

    add custom inline keyboard builder

--- keyboards/keyboards.py
+++ keyboards/keyboards.py
@@ -1,5 +1,5 @@
 from aiogram.types import KeyboardButton, ReplyKeyboardMarkup, InlineKeyboardMarkup, InlineKeyboardButton
-from aiogram.utils.keyboard import ReplyKeyboardBuilder
+from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder
 
 from lexicon.lexicon import LEXICON_RU
 from services.services import categories
@@ -13,20 +13,34 @@ common_keyboard_builder = ReplyKeyboardBuilder()
 common_keyboard_builder.row(button_choose_category, button_add_category,
                             button_delete_category, button_statistics, width=2)
 
+
+# Function for generating inline keyboards "on the fly"
+def create_inline_keyboard(width: int, *args: str, **kwargs: str) -> InlineKeyboardMarkup:
+    # Initialize the builder
+    kb_builder = InlineKeyboardBuilder()
+    buttons: list[InlineKeyboardButton] = []
+    # Fill the list with buttons from args and kwargs arguments
+    if args:
+        for button in args:
+            buttons.append(InlineKeyboardButton(
+                text=button,
+                callback_data=button
+            ))
+    if kwargs:
+        for button, text in kwargs.items():
+            buttons.append(InlineKeyboardButton(
+                text=text,
+                callback_data=button
+            ))
+    # Unpack the list with buttons into the builder using the row method with the width parameter
+    kb_builder.row(*buttons, width=width)
+    # Return the inline keyboard object
+    return kb_builder.as_markup()
+
+
 common_keyboard: ReplyKeyboardMarkup = common_keyboard_builder.as_markup(
     one_time_keyboard=True,
     resize_keyboard=True
 )
 
-
-big_button_1 = InlineKeyboardButton(
-    text='Категория_1',
-    callback_data='cat_1_pressed'
-)
-
-big_button_2 = InlineKeyboardButton(
-    text='Категория_2',
-    callback_data='cat_2_pressed'
-)
-categories_keyboard = InlineKeyboardMarkup(
-    inline_keyboard=[[big_button_1], [big_button_2]])
+categories_keyboard = create_inline_keyboard(2, *categories)
